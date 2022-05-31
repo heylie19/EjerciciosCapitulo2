afff7cdffc4b2093e52fe7e3cf829e77b18112f2
heylie19 <106571599+heylie19@users.noreply.github.com>
2022-05-30 22:58:43 -0600

    Add files via upload

---
+++ Ejercicio 2.20/Ejercicio220.java
@@ -0,0 +1,11 @@
+public class Ejercicio220 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("**");
+    }
+}
