1464e7b0b7eb6c98a49b81657731f6a4f5fcc07e
heylie19 <106571599+heylie19@users.noreply.github.com>
2022-05-30 23:00:03 -0600

    Add files via upload

---
+++ Ejercicio 2.27/Ejercicio227.java
@@ -0,0 +1,17 @@
+/*(Patrón de damas mediante asteriscos) Escriba una aplicación que muestre un patrón de tablero de damas*/
+
+public class Ejercicio227 
+{
+    public static void main(String arg[])
+    {   
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * * ");
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * * ");
+     
+    }  
+}       
