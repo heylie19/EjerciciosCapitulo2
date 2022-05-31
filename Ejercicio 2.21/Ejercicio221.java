37457e2754a595da4081e5a32a60ebf3ad154518
heylie19 <106571599+heylie19@users.noreply.github.com>
2022-05-30 22:59:07 -0600

    Add files via upload

---
+++ Ejercicio 2.21/Ejercicio221.java
@@ -0,0 +1,12 @@
+public class Ejercicio221
+{
+    public static void main(String[] args)
+    {
+        System.out.print("*");
+        System.out.print("***");
+        System.out.print("*****");
+        System.out.print("****");
+        System.out.println("**");
+    }
+}
+
---
+++ Ejercicio 2.23/Ejercicio223.java
@@ -0,0 +1,7 @@
+public class Ejercicio223 
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+    }
+}
